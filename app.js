68715cd67ce533263d590ad3ce8c6293b1099d6b
Franznic0 <franznicofranz@gmail.com>
2024-03-25 15:29:16 +0000

    Validation js

--- app.js
+++ app.js
@@ -27,65 +27,115 @@ const reset = document.querySelector('.complete-submit');
 
 // update values
 
-function updateValues() {
+    function updateValues() {
 
-    let nameExe = "Jane Appleseed";
-nameIn.oninput = () => {
-    if (nameIn.value == "") {
-        nameOut.innerText = nameExe;
-    } else {
-        nameOut.innerText = nameIn.value;
+        let nameExe = "Jane Appleseed";
+    nameIn.oninput = () => {
+        if (nameIn.value == "") {
+            nameOut.innerText = nameExe;
+        } else {
+            nameOut.innerText = nameIn.value;
+        }
     }
-}
 
-let numExe = "0000 0000 0000 0000";
-cardNumIn.oninput = () => {
-    if (cardNumIn.value == "") {
-        cardNumOut.innerText = numExe;
-    } else {
+    let numExe = "0000 0000 0000 0000";
+    cardNumIn.oninput = () => {
+        if (cardNumIn.value == "") {
+            cardNumOut.innerText = numExe;
+        } else {
 
-        
-        cardNumOut.innerText = cardNumIn.value;
+            
+            cardNumOut.innerText = cardNumIn.value;
+        }
     }
-}
 
-str.match(/(\d{4})/g).join(" ")
+    str.match(/(\d{4})/g).join(" ")
 
-let dateExe = "00";
-expMonthIn.oninput = () => {
-    if (expMonthIn.value == "") {
-        expMonthOut.innerText = dateExe;
-    } else {
-        expMonthOut.innerText = expMonthIn.value;
+    let dateExe = "00";
+    expMonthIn.oninput = () => {
+        if (expMonthIn.value == "") {
+            expMonthOut.innerText = dateExe;
+        } else {
+            expMonthOut.innerText = expMonthIn.value;
+        }
     }
-}
 
-expYearIn.oninput = () => {
-    if (expYearIn.value == "") {
-        expYearOut.innerText = dateExe;
-    } else {
-        expYearOut.innerText = expYearIn.value;
+    expYearIn.oninput = () => {
+        if (expYearIn.value == "") {
+            expYearOut.innerText = dateExe;
+        } else {
+            expYearOut.innerText = expYearIn.value;
+        }
     }
-}
 
-let cvcExe = "000";
-cvcIn.oninput = () => {
-    if (cvcIn.value == "") {
-        cvcOut.innerText = cvcExe;
-    } else {
-        cvcOut.innerText = cvcIn.value;
+    let cvcExe = "000";
+    cvcIn.oninput = () => {
+        if (cvcIn.value == "") {
+            cvcOut.innerText = cvcExe;
+        } else {
+            cvcOut.innerText = cvcIn.value;
+        }
     }
-}
 
-}
+    }
 
 // validate form
 
-function validateValue () {
+    // Luhn Algorithm
+    function validateLuhnAlgorithm(cardNumIn) {
+        let sum = 0;
+        let isEven = false;
 
-}
+        for (let i = cardNumIn.length - 1; i >= 0; i--) {
+            let digit = parseInt(cardNumIn.charAt(i), 10);
 
-// error message
+            if (isEven) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit -= 9;
+                }
+            }
+            sum += digit;
+            isEven = !isEven;
+        }
+        return sum % 10 === 0;
+    }
+
+    // Expiration Date Validation
+
+    function validateExpirationDate(expMonthIn, expYearIn) {
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        const currentMonth = currentDate.getMonth() + 1; // January is 0
+
+        if (expYearIn > currentYear) {
+            return true;
+        } else if (expYearIn === currentYear && expMonthIn >= currentMonth) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // CVC Validation
+
+    function validateCVV(cvcIn) {
+        const cvvPattern = /^[0-9]{3,4}$/;
+        return cvvPattern.test(cvcIn);
+    }
+
+    // Card Number Validation
+
+    cardNumIn.addEventListener("input", (e) => {
+        if (!validateLuhnAlgorithm(e.target.value)) {
+            cardNumIn.classList.add("error");
+            errNumber.innerHTML = "Invalid card number";
+        } else {
+            cardNumIn.classList.remove("error");
+            errNumber.innerHTML = "";
+        }
+    });
+
 
 // reset
 
